c565ebcfc85669c1dbe531b9fbe78b083b3a8227
aleksey.driuk <AlexDruyk@ukr.net>
2023-01-14 12:37:55 +0200

    added barcode generator

--- src/screens/home/home.tsx
+++ src/screens/home/home.tsx
@@ -1,11 +1,53 @@
-import { ReactElement } from 'react';
-import { Text, View } from 'react-native';
+import { ReactElement, useState } from 'react';
+import { Dimensions, Text, TextInput, View } from 'react-native';
+import Barcode from 'react-native-barcode-expo';
 
 export const HomeScreen = (): ReactElement => {
 
+  const [value, setValue] = useState('');
+
   return (
-    <View>
-      <Text>Home</Text>
+    <View
+      style={{
+        justifyContent: 'center',
+        alignItems: 'center',
+        flex: 1,
+      }}
+    >
+      <Text>Type value...</Text>
+      <TextInput
+        value={value}
+        onChangeText={setValue}
+        style={{
+          width: '50%',
+          backgroundColor: 'white',
+          paddingVertical: 5,
+          paddingHorizontal: 12,
+          borderWidth: 1,
+          borderColor: 'lightgrey',
+          marginBottom: 12,
+        }}
+      />
+
+      <View
+        style={{
+          width: '100%',
+          height: 120,
+          backgroundColor: 'white',
+        }}
+      >
+        {value &&
+          <Barcode
+            value={value}
+            format='CODE128' // CODE39
+            height={100}
+            width={2}
+            textColor='black'
+            lineColor='black'
+            background='white'
+          />
+        }
+      </View>
     </View>
   );
 };
